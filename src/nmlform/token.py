7a47b10069be9b10af94ef38c7378c4667aea0ae
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-07-21 14:07:50 -0700

    CLN: remove role + some unused Token bits

--- src/nmlform/token.py
+++ src/nmlform/token.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import enum
 import typing
 from typing import Any, ClassVar, SupportsIndex
 
@@ -14,17 +13,6 @@ if typing.TYPE_CHECKING:
         from typing_extensions import Self
 
 
-class Role(str, enum.Enum):
-    name_ = "name"
-    builtin = "builtin"
-    kind = "kind"
-    attribute_name = "attribute_name"
-    env_var = "env_var"
-    statement_definition = "statement_definition"
-    filename = "filename"
-    controller_variable = "controller_variable"
-
-
 class Token(str):
     """
     String with source loc information.
@@ -34,7 +22,6 @@ class Token(str):
 
     loc: Location
     comments: Comments
-    role: Role | None = None
     _upper: str
     _hash: int
 
@@ -45,7 +32,6 @@ class Token(str):
         content: str,
         loc: Location | None = None,
         comments: Comments | None = None,
-        role: Role | None = None,
     ):
         return super().__new__(cls, content)
 
@@ -54,26 +40,16 @@ class Token(str):
         content: str,
         loc: Location | None = None,
         comments: Comments | None = None,
-        role: Role | None = None,
     ):
         self.loc = loc or Location(end_column=len(content))
         self.comments = comments or Comments()
-        self.role = role
         self._upper = str.upper(self)
-        # self._hash = hash(self._upper)
-        self._hash = super().__hash__()
 
         # internal error
         if not isinstance(self.loc, Location):
             raise ValueError(type(self.loc))
         if not isinstance(self.comments, Comments):
             raise ValueError(type(self.comments))
-        if role and not isinstance(self.role, Role):
-            raise ValueError(type(self.role))
-
-    def __hash__(self):
-        # pydantic complains about mutable default otherwise
-        return self._hash
 
     def __eq__(self, other) -> bool:
         if isinstance(other, Token):
@@ -100,8 +76,9 @@ class Token(str):
         return self
 
     def __repr__(self) -> str:
+        parts: list[Any]
         if self._detailed_repr_:
-            parts = [super().__repr__(), self.role, self.loc]
+            parts = [super().__repr__(), self.loc]
         else:
             parts = [super().__repr__()]
         if bool(self.comments):
@@ -109,41 +86,11 @@ class Token(str):
         desc = ", ".join(str(part) for part in parts if part)
         return f"{type(self).__name__}({desc})"
 
-    def annotate(self, named: dict[Token, Any]):
-        if self.upper() in named and self.role not in {
-            Role.attribute_name,
-            Role.env_var,
-            Role.filename,
-        }:
-            self.role = Role.name_
-
-    def quoted(self) -> Self:
-        if self.is_quoted_string:
-            return self
-
-        value = str(self).strip()
-        if '"' in value:
-            quote_char = "'"
-        else:
-            quote_char = '"'
-        if quote_char in value:
-            # Sorry, but why use both quote types and not quote your string to begin with?
-            # I don't think there's an escape character we can use
-            value = value.replace(quote_char, " ")
-
-        return type(self)(
-            f"{quote_char}{value}{quote_char}",
-            loc=self.loc,
-            comments=self.comments,
-            role=self.role,
-        )
-
     def lstrip(self, chars: str | None = None) -> Self:
         return type(self)(
             str(self).lstrip(chars),
             loc=self.loc,
             comments=self.comments,
-            role=self.role,
         )
 
     def strip(self, chars: str | None = None) -> Self:
@@ -151,7 +98,6 @@ class Token(str):
             str(self).strip(chars),
             loc=self.loc,
             comments=self.comments,
-            role=self.role,
         )
 
     def replace(self, old, new, count: SupportsIndex = -1) -> Self:
@@ -159,7 +105,6 @@ class Token(str):
             str(self).replace(old, new, count),
             loc=self.loc,
             comments=self.comments,
-            role=self.role,
         )
 
     def with_(
@@ -167,13 +112,11 @@ class Token(str):
         *,
         loc: Location | None = None,
         comments: Comments | None = None,
-        role: Role | None = None,
     ):
         return type(self)(
             str(self),
             loc=loc or self.loc,
             comments=comments or self.comments,
-            role=role or self.role,
         )
 
     def upper(self):
@@ -181,7 +124,6 @@ class Token(str):
             self._upper,
             loc=self.loc,
             comments=self.comments,
-            role=self.role,
         )
 
     def lower(self):
@@ -189,7 +131,6 @@ class Token(str):
             super().lower(),
             loc=self.loc,
             comments=self.comments,
-            role=self.role,
         )
 
 
